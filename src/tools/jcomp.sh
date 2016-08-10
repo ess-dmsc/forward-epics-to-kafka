06fa1f993cea1acd74942e818f983c54caae0a3c
Dominik Werder <dominik.werder@psi.ch>
2016-08-10 11:15:18 +0200

    More flexible Topic creation helper

--- src/tools/jcomp.sh
+++ src/tools/jcomp.sh
@@ -1,7 +1,9 @@
+#KAFKA_LIBS=/home/scratch/software/kafka_2.11-0.10.0.0/libs
+#KAFKA_LIBS=/opt/kafka/libs
 javac -cp \
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/zkclient-0.8.jar:\
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/zookeeper-3.4.6.jar:\
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/kafka_2.11-0.10.0.0.jar:\
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/kafka-tools-0.10.0.0.jar:\
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/kafka-clients-0.10.0.0.jar \
+$KAFKA_LIBS/zkclient-0.8.jar:\
+$KAFKA_LIBS/zookeeper-3.4.6.jar:\
+$KAFKA_LIBS/kafka_2.11-0.10.0.0.jar:\
+$KAFKA_LIBS/kafka-tools-0.10.0.0.jar:\
+$KAFKA_LIBS/kafka-clients-0.10.0.0.jar \
 KafkaCreateTopics.java
--- src/tools/jrun.sh
+++ src/tools/jrun.sh
@@ -1,13 +1,16 @@
-/usr/lib/jvm/java-1.8.0-openjdk.x86_64/bin/java -cp \
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/zkclient-0.8.jar:\
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/zookeeper-3.4.6.jar:\
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/kafka_2.11-0.10.0.0.jar:\
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/kafka-tools-0.10.0.0.jar:\
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/kafka-clients-0.10.0.0.jar:\
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/log4j-1.2.17.jar:\
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/slf4j-log4j12-1.7.21.jar:\
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/slf4j-api-1.7.21.jar:\
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/scala-library-2.11.8.jar:\
-/home/scratch/software/kafka_2.11-0.10.0.0/libs/scala-parser-combinators_2.11-1.0.4.jar:\
+JAVA=/usr/lib/jvm/java-1.8.0-openjdk.x86_64/bin/java
+JAVA=java
+#KAFKA_LIBS=/opt/kafka/libs
+$JAVA -cp \
+$KAFKA_LIBS/zkclient-0.8.jar:\
+$KAFKA_LIBS/zookeeper-3.4.6.jar:\
+$KAFKA_LIBS/kafka_2.11-0.10.0.0.jar:\
+$KAFKA_LIBS/kafka-tools-0.10.0.0.jar:\
+$KAFKA_LIBS/kafka-clients-0.10.0.0.jar:\
+$KAFKA_LIBS/log4j-1.2.17.jar:\
+$KAFKA_LIBS/slf4j-log4j12-1.7.21.jar:\
+$KAFKA_LIBS/slf4j-api-1.7.21.jar:\
+$KAFKA_LIBS/scala-library-2.11.8.jar:\
+$KAFKA_LIBS/scala-parser-combinators_2.11-1.0.4.jar:\
 . \
 KafkaCreateTopics
