9bbeff4ac219473ca7da359c91074d89316a3135
Dominik Werder <dominik.werder@psi.ch>
2016-11-30 08:49:44 +0100

    Reduce logger api surface

--- src/logger.cxx
+++ src/logger.cxx
@@ -8,7 +8,7 @@
 
 int log_level = 3;
 
-FILE * log_file = stdout;
+static FILE * log_file = stdout;
 
 class Dummy {
 public:
@@ -22,7 +22,7 @@ Dummy() {
 	}
 }
 };
-Dummy dummy;
+static Dummy g__logger__dummy;
 
 
 int prefix_len() {
@@ -33,4 +33,21 @@ int prefix_len() {
 void use_log_file(std::string fname) {
 	FILE * f1 = fopen(fname.c_str(), "wb");
 	log_file = f1;
+}
+
+void dwlog_inner(int level, char const * file, int line, char const * func, std::string const & s1) {
+	int npre = prefix_len();
+	int const n2 = strlen(file);
+	if (npre > n2) {
+		fmt::print(log_file, "ERROR in logging API: npre > n2\n");
+		npre = 0;
+	}
+	auto f1 = file + npre;
+	try {
+		fmt::print(log_file, "{}:{} [{}]:  {}\n", f1, line, level, s1);
+	}
+	catch (fmt::FormatError & e) {
+		fmt::print(log_file, "ERROR  fmt::FormatError {}:{}\n", f1, line);
+	}
+	fflush(log_file);
 }
--- src/logger.h
+++ src/logger.h
@@ -1,57 +1,35 @@
 #pragma once
 
-#include <stdarg.h>
-#include <fmt/format.h>
 #include <string>
+#include <fmt/format.h>
 
-// Do we have a cross-platform way of doing this?
-// Would a stream work better?
 #ifdef _MSC_VER
 
 #define LOG(level, fmt, ...) { \
-	if (level >= log_level) { \
-		dwlog(level, fmt, __FILE__, __LINE__, __FUNCSIG__, __VA_ARGS__); \
-	} \
+	dwlog(level, fmt, __FILE__, __LINE__, __FUNCSIG__, __VA_ARGS__); \
 }
-
-//#define QLOG(level, fmt, ...) { dwlog(level, "[%lx] " fmt, __FILE__, __LINE__, __FUNCSIG__, (uint64_t)QThread::currentThreadId(), __VA_ARGS__); }
 
 #else
 
 #define LOG(level, fmt, args...) { \
-	if (level >= log_level) { \
-		dwlog(level, fmt, __FILE__, __LINE__, __PRETTY_FUNCTION__, ## args); \
-	} \
+	dwlog(level, fmt, __FILE__, __LINE__, __PRETTY_FUNCTION__, ## args); \
 }
-
-//#define QLOG(level, fmt, args...) { dwlog(level, "[%lx] " fmt, __FILE__, __LINE__, __PRETTY_FUNCTION__, (uint64_t)QThread::currentThreadId(), ## args); }
 
 #endif
 
 extern int log_level;
 
-int prefix_len();
-
-extern FILE * log_file;
+void dwlog_inner(int level, char const * file, int line, char const * func, std::string const & s1);
 
 template <typename ...TT>
 void dwlog(int level, char const * fmt, char const * file, int line, char const * func, TT const & ... args) {
-	// Here, we assume that this file is still called logger.cpp and lives in the root of the project
-	int npre = prefix_len();
-	int const n2 = strlen(file);
-	if (npre > n2) {
-		fmt::print(log_file, "ERROR in logging API: npre > n2\n");
-		npre = 0;
-	}
-	auto f1 = file + npre;
-	// TODO merge into one invocation..
+	if (level < log_level) return;
 	try {
-		fmt::print(log_file, "### {{{}}} {}:{}\n{}\n\n", level, f1, line, fmt::format(fmt, args...));
+		dwlog_inner(level, file, line, func, fmt::format(fmt, args...));
 	}
 	catch (fmt::FormatError & e) {
-		fmt::print(log_file, "ERROR  fmt::FormatError {}:{}\n", f1, line);
+		dwlog_inner(level, file, line, func, fmt::format("ERROR in format: {}: {}", e.what(), fmt));
 	}
-	fflush(log_file);
 }
 
 void use_log_file(std::string fname);
