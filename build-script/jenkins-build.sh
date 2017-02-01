0bbe7c92663a984f94972d82f7820046b9bce10c
Dominik Werder <dominik.werder@gmail.com>
2017-02-01 17:17:47 +0100

    Require gtest for jenkins build

--- build-script/jenkins-build.sh
+++ build-script/jenkins-build.sh
@@ -3,4 +3,4 @@ pwd
 mkdir build
 mkdir install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=`cd ../install; pwd` ../repos/forward-epics-to-kafka
+cmake -DCMAKE_INSTALL_PREFIX=`cd ../install; pwd` -DREQUIRE_GTEST=1 ../repos/forward-epics-to-kafka
