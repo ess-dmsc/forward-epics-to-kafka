0037b25755e19c70a5bc568a2a66409168e2b83c
Dominik Werder <dominik.werder@psi.ch>
2017-02-06 11:00:21 +0100

    Discover more of the env

--- build-script/jenkins-build.sh
+++ build-script/jenkins-build.sh
@@ -1,25 +1,43 @@
 echo '-----------------------------------------------------'
 echo '-----------------------------------------------------'
-echo '-----------------------------------------------------'
+echo find /opt/epics
 find /opt/epics -maxdepth 3
 echo '-----------------------------------------------------'
 echo '-----------------------------------------------------'
-echo '-----------------------------------------------------'
+echo find DM_ROOT/usr
 find $DM_ROOT/usr -maxdepth 2
 echo '-----------------------------------------------------'
 echo '-----------------------------------------------------'
-echo '-----------------------------------------------------'
+echo find /usr/local
 find /usr/local -maxdepth 2
 echo '-----------------------------------------------------'
 echo '-----------------------------------------------------'
-echo '-----------------------------------------------------'
+echo find EPICS_BASES_PATH
 find $EPICS_BASES_PATH -maxdepth 2
 echo '-----------------------------------------------------'
 echo '-----------------------------------------------------'
+echo find EPICS_BASE
 find $EPICS_BASE -maxdepth 2
 echo '-----------------------------------------------------'
 echo '-----------------------------------------------------'
+echo EPICS_HOST_ARCH
 echo $EPICS_HOST_ARCH
+echo '-----------------------------------------------------'
+echo '-----------------------------------------------------'
+echo /opt/epics/modules/pvDataCPP
+find /opt/epics/modules/pvDataCPP -maxdepth 2
+echo '-----------------------------------------------------'
+echo '-----------------------------------------------------'
+echo /opt/epics/modules/pvAccessCPP
+find /opt/epics/modules/pvAccessCPP -maxdepth 2
+echo '-----------------------------------------------------'
+echo '-----------------------------------------------------'
+echo /opt/epics/modules/pvDatabaseCPP
+find /opt/epics/modules/pvDatabaseCPP -maxdepth 2
+echo '-----------------------------------------------------'
+echo '-----------------------------------------------------'
+echo /opt/epics/modules/normativeTypesCPP
+find /opt/epics/modules/normativeTypesCPP -maxdepth 2
 echo '-----------------------------------------------------'
 echo '-----------------------------------------------------'
 echo "Build script start"
@@ -27,4 +45,4 @@ pwd
 mkdir build
 mkdir install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=`cd ../install; pwd` -DREQUIRE_GTEST=1 ../repos/forward-epics-to-kafka
+cmake -DCMAKE_INSTALL_PREFIX=`cd ../install; pwd` -DREQUIRE_GTEST=0 ../repos/forward-epics-to-kafka
