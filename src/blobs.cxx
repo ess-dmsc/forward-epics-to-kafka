648b3ee734c1769a288d2b9f82abdcb655452486
Dominik Werder <dominik.werder@psi.ch>
2017-02-24 09:08:54 +0100

    Schema data for validation of configuration json

---
+++ src/blobs.cxx
@@ -0,0 +1,12 @@
+#include "blobs.h"
+namespace BrightnESS {
+namespace ForwardEpicsToKafka {
+namespace blobs {
+
+std::vector<char> schema_config_global_json {
+#include "schema-config-global.json.cxx"
+};
+
+}
+}
+}
