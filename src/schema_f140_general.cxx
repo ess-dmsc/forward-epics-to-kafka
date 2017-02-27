ddb6c0fd9dbba964a46883c460f0c24cfec502df
Jonas Nilsson <jonas.nilsson@esss.se>
2017-02-27 16:38:16 +0100

    Fixed compilation error in clang.

--- src/schema_f140_general.cxx
+++ src/schema_f140_general.cxx
@@ -193,17 +193,17 @@ namespace fbg {
 
 std::vector<char> binary_to_hex(char const * data, int len) {
 	std::vector<char> ret;
+    ret.reserve(len*2);
 	for (int i1 = 0; i1 < len; ++i1) {
 		auto c = (uint8_t)data[i1];
-		for (auto & v : std::array<uint8_t,2>({{(uint8_t)(c>>4), (uint8_t)(c & 0x0f)}})) {
-			if (v < 10) v += 48;
-			else v += 97 - 10;
-			ret.push_back(v);
-		}
-		if (i1 % 8 == 7) ret.push_back(' ');
+        std::stringstream stream;
+        stream << std::setfill ('0') << std::setw(2) << std::hex << int(c);
+        ret.push_back(stream.str()[0]);
+        ret.push_back(stream.str()[1]);
 	}
 	return ret;
 }
+
 
 
 
