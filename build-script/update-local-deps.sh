6b1c9194adbd7628c7e59138951dd6c8f5e5e761
Dominik Werder <dominik.werder@psi.ch>
2017-03-17 14:20:46 +0100

    Update Jenkins build script, kick off build

--- build-script/update-local-deps.sh
+++ build-script/update-local-deps.sh
@@ -1,3 +1,5 @@
+BDIR=`pwd`
+
 if [ -d "streaming-data-types" ]
 then
 
@@ -10,5 +12,21 @@ fi
 
 else
 git clone -b master https://github.com/ess-dmsc/streaming-data-types.git
+fi
 
+
+cd $BDIR
+
+if [ -d "googletest" ]
+then
+
+if [ ! -d "googletest/.git" ]
+then
+echo "THIS LOOKS LIKE A BROKEN BUILD DIRECTORY.  SHOULD WIPE EVERYTHING."
+else
+cd googletest && git pull
+fi
+
+else
+git clone -b release-1.8.0 https://github.com/google/googletest.git
 fi
