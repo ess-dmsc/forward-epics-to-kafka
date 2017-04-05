dcfd749ed4b94ba6ba3e94b5601cbac812aa6b57
Dominik Werder <dominik.werder@psi.ch>
2017-04-05 15:22:37 +0200

    Test if paths are still necessary on official build node

--- build-script/invoke-cmake-from-jenkinsfile.sh
+++ build-script/invoke-cmake-from-jenkinsfile.sh
@@ -1,5 +1,9 @@
 # Factored out from Jenkinsfile because of escape issues
 
+echo 'Epics env vars:'
+set | grep -i epics
+echo ''
+
 cmake ../code \
 -DCMAKE_INCLUDE_PATH=../googletest\;../streaming-data-types\;$DM_ROOT/usr/include\;$DM_ROOT/usr/lib \
 -DCMAKE_LIBRARY_PATH=$DM_ROOT/usr/lib \
