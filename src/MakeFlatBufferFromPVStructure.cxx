45f8a4aafd86b05cc2407d5f9d50fba30806ae6b
Dominik Werder <dominik.werder@psi.ch>
2017-06-01 08:06:02 +0200

    Add dtor to conversion handler

--- src/MakeFlatBufferFromPVStructure.cxx
+++ src/MakeFlatBufferFromPVStructure.cxx
@@ -3,6 +3,8 @@
 namespace BrightnESS {
 namespace FlatBufs {
 
+MakeFlatBufferFromPVStructure::~MakeFlatBufferFromPVStructure() {}
+
 void MakeFlatBufferFromPVStructure::config(
     std::map<std::string, int64_t> const &config_ints,
     std::map<std::string, std::string> const &config_strings) {}
