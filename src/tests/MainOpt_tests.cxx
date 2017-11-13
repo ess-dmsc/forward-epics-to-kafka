c8582e70f57a8b41fc554384a119c07b55feeae0
Jack Harper <jack.harper@stfc.ac.uk>
2017-11-13 15:56:58 +0000

    Update MainOpt_tests.cxx

--- src/tests/MainOpt_tests.cxx
+++ src/tests/MainOpt_tests.cxx
@@ -11,7 +11,7 @@ public:
 TEST_F(MainOpt_T, parse_config_file) {
   MainOpt opt;
 
-  ASSERT_EQ(opt.parse_json_file("test-config-valid.json"), 0);
+  ASSERT_EQ(opt.parse_json_file("tests/test-config-valid.json"), 0);
   ASSERT_TRUE(opt.brokers.size() == 2);
   ASSERT_EQ(opt.brokers.at(0).host_port, "localhost:9092");
   ASSERT_EQ(opt.brokers.at(1).host_port, "127.0.0.1:9092");
