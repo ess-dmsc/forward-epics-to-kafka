ffd7c91b9d904a01b280877334429f2df0035474
rerpha <jack.harper@stfc.ac.uk>
2017-11-21 12:10:58 +0000

    review changes

--- src/tests/helper_tests.cxx
+++ src/tests/helper_tests.cxx
@@ -20,15 +20,13 @@ TEST(HelperTests, split_with_empty_token) {
 }
 
 TEST(HelperTests, split_with_empty_string) {
-  std::vector<std::string> expected = {};
   auto actual = split("", ",");
-  ASSERT_EQ(expected, actual);
+  ASSERT_TRUE(actual.empty());
 }
 
 TEST(HelperTests, split_with_empty_string_and_token) {
-  std::vector<std::string> expected = {""};
   auto actual = split("", "");
-  ASSERT_EQ(expected, actual);
+  ASSERT_TRUE(actual.empty());
 }
 
 TEST(HelperTests, split_with_character_not_in_string) {
@@ -44,9 +42,8 @@ TEST(HelperTests, split_with_extended_ascii_token) {
 }
 
 TEST(HelperTests, gulp_returns_empty_vector_given_string) {
-  std::vector<char> expected = {};
   auto actual = gulp("1");
-  ASSERT_EQ(expected, actual);
+  ASSERT_TRUE(actual.empty());
 }
 
 TEST(HelperTests, gulp_returns_correct_binary_when_file_is_read) {
@@ -59,9 +56,8 @@ TEST(HelperTests, gulp_returns_correct_binary_when_file_is_read) {
 }
 
 TEST(HelperTests, gulp_returns_empty_vector_when_empty_file_is_given) {
-  std::vector<char> expected = {};
   auto actual = gulp("");
-  ASSERT_EQ(expected, actual);
+  ASSERT_TRUE(actual.empty());
 }
 
 TEST(HelperTests, get_string_returns_exit_command) {
@@ -123,70 +119,56 @@ TEST(HelperTests, get_string_returns_number_given_number_in_array) {
 }
 
 TEST(HelperTests, get_string_returns_empty_string_if_array_is_empty) {
-  std::string expected;
   rapidjson::Document j0;
   j0.Parse("[]");
   std::string actual = get_string(&j0, "1");
 
-  ASSERT_EQ(expected, actual);
+  ASSERT_TRUE(actual.empty());
 }
 
 TEST(HelperTests, get_string_returns_empty_string_if_empty_json) {
-  std::string expected;
   rapidjson::Document j0;
   j0.Parse("");
   std::string actual = get_string(&j0, "1");
 
-  ASSERT_EQ(expected, actual);
+  ASSERT_TRUE(actual.empty());
 }
 
 TEST(HelperTests, split_with_empty_string_returns_empty_vector) {
-  using std::vector;
-  using std::string;
   auto v = split("", "");
-  ASSERT_TRUE(v == vector<string>({""}));
+  ASSERT_TRUE(v.empty());
 }
 
 TEST(HelperTests,
      split_with_no_character_returns_vector_containing_whole_string) {
-  using std::vector;
-  using std::string;
   auto v = split("abc", "");
-  ASSERT_TRUE(v == vector<string>({"abc"}));
+  ASSERT_EQ(v, std::vector<std::string>({"abc"}));
 }
 
 TEST(
     HelperTests,
     split_returns_vector_with_two_words_in_with_split_character_before_and_after_words) {
-  using std::vector;
-  using std::string;
   auto v = split("/a/b", "/");
-  ASSERT_TRUE(v == vector<string>({"a", "b"}));
+  ASSERT_EQ(v, std::vector<std::string>({"a", "b"}));
 }
 
 TEST(
     HelperTests,
     split_does_not_split_all_characters_and_returns_vector_of_words_between_split_character) {
-  using std::vector;
-  using std::string;
   auto v = split("ac/dc/", "/");
-  ASSERT_TRUE(v == vector<string>({"ac", "dc"}));
+  ASSERT_EQ(v, std::vector<std::string>({"ac", "dc"}));
 }
 
 TEST(HelperTests,
      split_adds_no_blank_characters_with_character_before_and_after_string) {
-  using std::vector;
-  using std::string;
   auto v = split("/ac/dc/", "/");
-  ASSERT_TRUE(v == vector<string>({"ac", "dc"}));
+  ASSERT_EQ(v, std::vector<std::string>({"ac", "dc"}));
 }
 
 TEST(HelperTests, split_adds_multiple_words_before_and_after_characters) {
-  using std::vector;
-  using std::string;
   auto v = split("/some/longer/thing/for/testing", "/");
-  ASSERT_TRUE(v ==
-              vector<string>({"some", "longer", "thing", "for", "testing"}));
+  ASSERT_EQ(v,
+              std::vector<std::string>({"some", "longer", "thing", "for", "testing"}));
 }
 
 TEST(Sleep, sleep_ms) {
@@ -195,27 +177,26 @@ TEST(Sleep, sleep_ms) {
 }
 
 TEST(RapidTools, get_string_01) {
-  using namespace rapidjson;
-  Document d;
+  rapidjson::Document d;
   d.SetObject();
   auto &a = d.GetAllocator();
-  d.AddMember("mem00", Value("s1", a), a);
-  Value v2;
+  d.AddMember("mem00", rapidjson::Value("s1", a), a);
+  rapidjson::Value v2;
   v2.SetObject();
-  v2.AddMember("mem10", Value("s2", a), a);
+  v2.AddMember("mem10", rapidjson::Value("s2", a), a);
   d.AddMember("mem01", v2.Move(), a);
 
   {
-    Value va;
+    rapidjson::Value va;
     va.SetArray();
-    va.PushBack(Value("something_a_0", a), a);
-    va.PushBack(Value("something_a_1", a), a);
-    va.PushBack(Value(1234), a);
+    va.PushBack(rapidjson::Value("something_a_0", a), a);
+    va.PushBack(rapidjson::Value("something_a_1", a), a);
+    va.PushBack(rapidjson::Value(1234), a);
     d.AddMember("mem02", va, a);
   }
 
-  StringBuffer buf1;
-  PrettyWriter<StringBuffer> wr(buf1);
+  rapidjson::StringBuffer buf1;
+  rapidjson::PrettyWriter<rapidjson::StringBuffer> wr(buf1);
   d.Accept(wr);
   auto s1 = get_string(&d, "mem00");
   ASSERT_EQ(s1, "s1");
