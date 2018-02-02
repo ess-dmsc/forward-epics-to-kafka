ess-dmsc/forward-epics-to-kafka

commit 1f3a1b734755c10dd8ba30f4e5b1bce929b565e2

adding testfor config listener and restructuring kafkaw consumer

src/KafkaW.cxx
@@ -149,7 +149,7 @@ PollStatus PollStatus::Empty() {
   return ret;
 }
 
-PollStatus PollStatus::make_Msg(std::unique_ptr<Msg> x) {
+PollStatus PollStatus::make_Msg(std::unique_ptr<AbstractMsg> x) {
   PollStatus ret;
   ret.state = 1;
   ret.data = x.release();
@@ -186,9 +186,9 @@ bool PollStatus::is_EOP() { return state == -2; }
 
 bool PollStatus::is_Empty() { return state == -3; }
 
-std::unique_ptr<Msg> PollStatus::is_Msg() {
+std::unique_ptr<AbstractMsg> PollStatus::is_Msg() {
   if (state == 1) {
-    std::unique_ptr<Msg> ret((Msg *)data);
+    std::unique_ptr<AbstractMsg> ret((AbstractMsg *)data);
     data = nullptr;
     return ret;
   }
@@ -196,23 +196,12 @@ std::unique_ptr<Msg> PollStatus::is_Msg() {
 }
 
 Consumer::Consumer(BrokerOpt opt) : opt(opt) {
-  // on_rebalance_start = nullptr;
-  // on_rebalance_assign = nullptr;
+  on_rebalance_assign = {};
+  on_rebalance_start = {};
+  add_topic(topic);
   init();
   id = g_kafka_instance_count++;
 }
-
-/*
-Consumer::Consumer(Consumer && x) {
-        using std::swap;
-        swap(on_rebalance_assign, x.on_rebalance_assign);
-        swap(on_rebalance_start, x.on_rebalance_start);
-        swap(rk, x.rk);
-        swap(opt, x.opt);
-        swap(plist, x.plist);
-        swap(id, x.id);
-}
-*/
 
 Consumer::~Consumer() {
   LOG(7, "~Consumer()");
@@ -409,7 +398,7 @@ PollStatus Consumer::poll() {
   }
 
   static_assert(sizeof(char) == 1, "Failed: sizeof(char) == 1");
-  std::unique_ptr<Msg> m2(new Msg);
+  std::unique_ptr<AbstractMsg> m2(new Msg);
   m2->kmsg = msg;
   // auto topic_name = rd_kafka_topic_name(msg->rkt);
   // int partition = msg->partition;

src/Config.cxx
@@ -3,23 +3,15 @@
 #include <condition_variable>
 #include <memory>
 #include <mutex>
+#include <iostream>
 
 namespace BrightnESS {
 namespace ForwardEpicsToKafka {
 namespace Config {
 
-struct Listener_impl {
-  std::unique_ptr<KafkaW::Consumer> consumer;
-  std::mutex mx;
-  std::condition_variable cv;
-  int connected = 0;
-};
-
-Listener::Listener(KafkaW::BrokerOpt bopt, uri::URI uri) {
-  bopt.address = uri.host_port;
-  bopt.poll_timeout_ms = 0;
+Listener::Listener(std::unique_ptr<KafkaW::BaseConsumer> baseConsumer) {
   impl.reset(new Listener_impl);
-  impl->consumer.reset(new KafkaW::Consumer(bopt));
+  impl->consumer.swap(baseConsumer);
   auto &consumer = *impl->consumer;
   consumer.on_rebalance_assign =
       [this](rd_kafka_topic_partition_list_t *plist) {
@@ -29,9 +21,6 @@ Listener::Listener(KafkaW::BrokerOpt bopt, uri::URI uri) {
         }
         impl->cv.notify_all();
       };
-  consumer.on_rebalance_assign = {};
-  consumer.on_rebalance_start = {};
-  consumer.add_topic(uri.topic);
 }
 
 Listener::~Listener() {}

src/Config.h
@@ -2,11 +2,13 @@
 
 #include "KafkaW.h"
 #include "uri.h"
+#include "helper.h"
 #include <atomic>
 #include <chrono>
 #include <memory>
 #include <string>
 #include <vector>
+#include <condition_variable>
 
 namespace BrightnESS {
 namespace ForwardEpicsToKafka {
@@ -23,11 +25,16 @@ public:
   virtual void operator()(string const &msg) = 0;
 };
 
-struct Listener_impl;
+struct Listener_impl{
+  std::unique_ptr<KafkaW::BaseConsumer> consumer;
+  std::mutex mx;
+  std::condition_variable cv;
+  int connected = 0;
+};
 
 class Listener {
 public:
-  Listener(KafkaW::BrokerOpt bopt, uri::URI uri);
+  Listener(std::unique_ptr<KafkaW::BaseConsumer> baseConsumer);
   Listener(Listener const &) = delete;
   ~Listener();
   void poll(Callback &cb);

src/KafkaW.h
@@ -44,15 +44,25 @@ public:
   std::map<std::string, std::string> conf_strings;
 };
 
-class Msg {
+class AbstractMsg {
+public:
+  virtual uchar *data()=0;
+  virtual uint32_t size()=0;
+  void *kmsg = nullptr;
+  virtual char const *topic_name()=0;
+  virtual int32_t offset()=0;
+  virtual int32_t partition()=0;
+};
+
+class Msg : public AbstractMsg {
 public:
   ~Msg();
-  uchar *data();
-  uint32_t size();
+  uchar *data() override;
+  uint32_t size() override;
   void *kmsg;
-  char const *topic_name();
-  int32_t offset();
-  int32_t partition();
+  char const *topic_name() override;
+  int32_t offset() override;
+  int32_t partition() override;
 };
 
 class PollStatus {
@@ -61,7 +71,7 @@ public:
   static PollStatus Err();
   static PollStatus EOP();
   static PollStatus Empty();
-  static PollStatus make_Msg(std::unique_ptr<Msg> x);
+  static PollStatus make_Msg(std::unique_ptr<AbstractMsg> x);
   PollStatus(PollStatus &&);
   PollStatus &operator=(PollStatus &&);
   ~PollStatus();
@@ -71,7 +81,7 @@ public:
   bool is_Err();
   bool is_EOP();
   bool is_Empty();
-  std::unique_ptr<Msg> is_Msg();
+  virtual std::unique_ptr<AbstractMsg> is_Msg();
 
 private:
   int state = -1;
@@ -80,21 +90,29 @@ private:
 
 class Inspect;
 
-class Consumer {
+class BaseConsumer{
 public:
-  Consumer(BrokerOpt opt);
-  Consumer(Consumer &&) = delete;
-  Consumer(Consumer const &) = delete;
+  virtual ~BaseConsumer() {}
+  virtual void init()=0;
+  virtual void add_topic(std::string topic)=0;
+  virtual PollStatus poll()=0;
+  std::function<void(rd_kafka_topic_partition_list_t *plist)> on_rebalance_assign;
+  std::function<void(rd_kafka_topic_partition_list_t *plist)> on_rebalance_start;
+  std::string topic;
+};
+
+class Consumer : public BaseConsumer {
+public:
+  explicit Consumer(BrokerOpt opt);
+  Consumer(Consumer &&) = default;
+  Consumer(Consumer const &) = default;
   ~Consumer();
-  void init();
-  void add_topic(std::string topic);
+  void init() override;
+  void add_topic(std::string topic) override;
   void dump_current_subscription();
-  PollStatus poll();
-  std::function<void(rd_kafka_topic_partition_list_t *plist)>
-      on_rebalance_assign;
-  std::function<void(rd_kafka_topic_partition_list_t *plist)>
-      on_rebalance_start;
+  PollStatus poll() override;
   rd_kafka_t *rk = nullptr;
+  std::string topic;
 
 private:
   BrokerOpt opt;

src/Main.cxx
@@ -93,7 +93,13 @@ Main::Main(MainOpt &opt)
     KafkaW::BrokerOpt bopt;
     bopt.conf_strings["group.id"] =
         fmt::format("forwarder-command-listener--pid{}", getpid());
-    config_listener.reset(new Config::Listener{bopt, main_opt.broker_config});
+
+    bopt.address = main_opt.broker_config.host_port;
+    bopt.poll_timeout_ms = 0;
+
+    auto consumer_ptr = make_unique<KafkaW::Consumer>(bopt);
+    consumer_ptr->topic = main_opt.broker_config.host;
+    config_listener =  make_unique<Config::Listener>(std::move(consumer_ptr));
   }
   if (main_opt.json) {
     auto m1 = main_opt.json->FindMember("streams");

src/tests/CMakeLists.txt
@@ -8,6 +8,7 @@ set(sources
         helper.cxx
         uri.cxx
         streams_tests.cxx
+        listener_tests.cpp
         $<TARGET_OBJECTS:__objects>
 )
 add_executable(${tgt} ${sources})

src/tests/listener_tests.cpp
@@ -1,0 +1,107 @@
+#include "gmock/gmock.h"
+#include "../KafkaW.h"
+#include "../Config.h"
+#include "../uri.h"
+#include "../helper.h"
+#include <mutex>
+#include <condition_variable>
+using namespace testing;
+
+class cb_fake : public BrightnESS::ForwardEpicsToKafka::Config::Callback {
+public:
+  void operator()(std::string const &msg) override {m_message = msg;};
+  std::string get_message(){return m_message;};
+private:
+  std::string m_message;
+};
+
+class FakeMsg : public KafkaW::AbstractMsg {
+  using uchar = unsigned char;
+  unsigned char some_data[4] = {'t', 'e', 's', 't'};
+public:
+  FakeMsg(std::string msg) : KafkaW::AbstractMsg() {
+    kmsg = static_cast<void*>(&msg);
+  }
+
+  FakeMsg() = default;
+
+  uchar *data() override {
+    return some_data;
+  }
+
+  uint32_t size() override {
+    uint32_t s = 4;
+    return s;
+  }
+
+  char const *topic_name() override {return "test_topic_name";};
+  int32_t offset() override {return 1;};
+  int32_t partition() override {return 1;};
+
+};
+
+
+class FakePollStatus : public KafkaW::PollStatus {
+public:
+  std::unique_ptr<KafkaW::AbstractMsg> is_Msg() override {
+    FakeMsg mesg("test");
+    return make_unique<FakeMsg>(mesg);
+  };
+//  static FakePollStatus make_Msg(std::unique_ptr<KafkaW::Msg> x)=default;
+
+private:
+  int state = -1;
+  void *data = nullptr;
+};
+
+class MockConsumer : public KafkaW::BaseConsumer {
+public:
+  MockConsumer()=default;
+  virtual ~MockConsumer(){};
+  MockConsumer(const MockConsumer&){};
+  MOCK_METHOD0(init, void());
+  MOCK_METHOD1(add_topic, void(std::string topic));
+  MockConsumer& operator=(const MockConsumer)=delete;
+
+  KafkaW::PollStatus poll() override {
+    std::unique_ptr<FakeMsg> m2(new FakeMsg);
+    m2->kmsg = (void *) "hello";
+    return FakePollStatus::make_Msg(std::move(m2));
+//    auto msg = make_unique<KafkaW::Msg>();
+//    return status.make_Msg(msg);
+  }
+};
+
+//
+//TEST(kafkaw_consumer_tests, test_something) {
+//  MockConsumer mc;
+//  EXPECT_CALL(mc, poll()).Times(AtLeast(1));
+//  mc.poll();
+//}
+//
+//TEST(kafkaw_consumer_tests, test_something_else) {
+//  MockConsumer mc;
+//  EXPECT_CALL(mc, poll()).Times(AtLeast(1));
+//  mc.init();
+//}
+
+//TEST(kafkaw_consumer_tests, test_config_polling) {
+//
+////  EXPECT_CALL(mc, poll()).Times(AtLeast(1));
+////  std::unique_ptr<KafkaW::BaseConsumer> ptr = make_unique<MockConsumer>(mc);
+//  auto ptr = std::make_shared<MockConsumer>();
+//  EXPECT_CALL(*ptr, poll());
+//  BrightnESS::ForwardEpicsToKafka::Config::Listener listener1(ptr);
+//  auto cb = cb_fake();
+//  listener1.poll(cb);
+//  std::cout << cb.get_message() << std::endl;
+//}
+
+TEST(kafkaw_consumer_tests, test_config_polling_returns_msg) {
+  std::string expected = "test";
+  auto consumer_ptr = make_unique<MockConsumer>();
+  BrightnESS::ForwardEpicsToKafka::Config::Listener listener1(std::move(consumer_ptr));
+  auto cb = cb_fake();
+  listener1.poll(cb);
+  ASSERT_EQ(std::string(cb.get_message()), expected);
+}

src/tests/streams_tests.cxx
@@ -153,7 +153,7 @@ TEST(StreamsTest, channel_stop_removes_no_channels_with_no_matched_channel_name)
   ASSERT_EQ(2, streams.size());
 }
 
-TEST(StreamsTest, channel_stop_removes_no_channels_with_no__given_channel_name) {
+TEST(StreamsTest, channel_stop_removes_no_channels_with_no_given_channel_name) {
   Streams streams;
   auto s = createMockStream("hello", "world");
   auto s2 = createMockStream("world", "world");

commit 60cb204d4cd4c06f9e9584beb22a0e654b857877

Fix regression introduced by 1f3a1b73
In Consumer::Consumer(...) code was added by `1f3a1b73` before the init() call
which resulted in the usage of the members of the incompletely initialized
class.

src/KafkaW.cxx
@@ -196,9 +196,6 @@ std::unique_ptr<AbstractMsg> PollStatus::is_Msg() {
 }
 
 Consumer::Consumer(BrokerOpt opt) : opt(opt) {
-  on_rebalance_assign = {};
-  on_rebalance_start = {};
-  add_topic(topic);
   init();
   id = g_kafka_instance_count++;
 }
@@ -320,6 +317,8 @@ void Consumer::cb_rebalance(rd_kafka_t *rk, rd_kafka_resp_err_t err,
 }
 
 void Consumer::init() {
+  on_rebalance_assign = {};
+  on_rebalance_start = {};
   // librdkafka API sometimes wants to write errors into a buffer:
   int const errstr_N = 512;
   char errstr[errstr_N];