a0125a9872687a3cbc78c06a687156eab71594fa
Dominik Werder <dominik.werder@psi.ch>
2018-04-06 10:32:46 +0200

    Change comment style
    Updates #58

--- src/fbschemas.cpp
+++ src/fbschemas.cpp
@@ -7,38 +7,32 @@ namespace FlatBufs {
 
 static_assert(FLATBUFFERS_LITTLEENDIAN, "We require little endian.");
 
-/**
-\class FB
-\brief
-Holds the flatbuffer until it has been sent.
+/// \class FB
+/// \brief
+/// Holds the flatbuffer until it has been sent.
+///
+/// Basically POD.  Holds the flatbuffer until no longer needed.
+/// Also holds some internal testing data.
+/// If you want to implement your own custom memory management, this is the
+/// class to inherit from.
 
-Basically POD.  Holds the flatbuffer until no longer needed.
-Also holds some internal testing data.
-If you want to implement your own custom memory management, this is the
-class to inherit from.
-*/
+/// Gives a standard FlatBufferBuilder.
 
-/**
-Gives a standard FlatBufferBuilder.
-*/
 FB::FB() : builder(new flatbuffers::FlatBufferBuilder()) {}
 
-/**
-\brief FlatBufferBuilder with initial_size in bytes.
-\param initial_size Initial size of the FlatBufferBuilder in bytes.
-*/
+/// \brief FlatBufferBuilder with initial_size in bytes.
+/// \param initial_size Initial size of the FlatBufferBuilder in bytes.
+
 FB::FB(uint32_t initial_size)
     : builder(new flatbuffers::FlatBufferBuilder(initial_size)) {}
 
-/**
-\brief Your chance to implement your own memory recycling.
-*/
+/// \brief Your chance to implement your own memory recycling.
+
 FB::~FB() {}
 
-/**
-Returns the underlying data of the flatbuffer.
-Called when actually writing to Kafka.
-*/
+/// Returns the underlying data of the flatbuffer.
+/// Called when actually writing to Kafka.
+
 FlatbufferRawMessageSlice FB::message() {
   if (!builder) {
     CLOG(8, 1, "builder no longer available");
