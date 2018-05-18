6ba6920f56606b8da96fd928df2b075a5b9147e2
Matt Clarke <matt.clarke@esss.se>
2018-05-18 13:20:03 +0200

    Marked constructor explicit

--- src/CommandHandler.h
+++ src/CommandHandler.h
@@ -9,7 +9,7 @@ namespace ForwardEpicsToKafka {
 /// \brief Helper class to provide a callback for the Kafka command listener.
 class ConfigCB : public Config::Callback {
 public:
-  ConfigCB(Main &main);
+  explicit ConfigCB(Main &main);
   // This is called from the same thread as the main watchdog because the
   // code below calls the config poll which in turn calls this callback.
   void operator()(std::string const &msg) override;
