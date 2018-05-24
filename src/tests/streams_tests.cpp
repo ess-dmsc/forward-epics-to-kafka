1cb97f50cdfaffc79368cb63f1b6fb57c9991db7
rerpha <jack.harper@stfc.ac.uk>
2018-05-24 11:50:09 +0100

    re #93 clang-formatting

--- src/tests/streams_tests.cpp
+++ src/tests/streams_tests.cpp
@@ -16,7 +16,9 @@ class EpicsPVUpdate;
 
 class FakeEpicsClient : public EpicsClient::EpicsClientInterface {
 public:
-  int emit(std::unique_ptr<BrightnESS::FlatBufs::EpicsPVUpdate> up) override { return 0; };
+  int emit(std::unique_ptr<BrightnESS::FlatBufs::EpicsPVUpdate> up) override {
+    return 0;
+  };
   int stop() override { return 0; };
   void error_in_epics() override { status_ = -1; };
   int status() override { return status_; };
@@ -26,9 +28,9 @@ private:
 };
 
 std::shared_ptr<Stream> createStream(std::string provider_type,
-                                         std::string channel_name) {
-  auto ring =
-    std::make_shared<Ring<std::unique_ptr<BrightnESS::FlatBufs::EpicsPVUpdate>>>();
+                                     std::string channel_name) {
+  auto ring = std::make_shared<
+      Ring<std::unique_ptr<BrightnESS::FlatBufs::EpicsPVUpdate>>>();
   auto client =
       std::unique_ptr<EpicsClient::EpicsClientInterface>(new FakeEpicsClient());
   ChannelInfo ci{std::move(provider_type), std::move(channel_name)};
