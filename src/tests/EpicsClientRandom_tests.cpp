e405dd14a2934750632539fc83a2168d75ef11ed
Matthew D Jones <matthew.d.jones@tessella.com>
2018-06-13 15:57:24 +0100

    Re #112 Test EpicsClientRandom generates EpicsPVUpdates with different values

--- src/tests/EpicsClientRandom_tests.cpp
+++ src/tests/EpicsClientRandom_tests.cpp
@@ -1,16 +1,61 @@
+#include "../EpicsClient/EpicsClientRandom.h"
+#include "EpicsPVUpdate.h"
+#include "Ring.h"
 #include <gtest/gtest.h>
 #include <memory>
-#include "Ring.h"
-#include "EpicsPVUpdate.h"
-#include "../EpicsClient/EpicsClientRandom.h"
 
 using namespace Forwarder;
 
-TEST(EpicsClientRandomTests, CallingGeneratePVUpdateResultsInAPVUpdateInTheBuffer) {
-  auto RingBuffer = std::make_shared<Ring<std::unique_ptr<FlatBufs::EpicsPVUpdate>>>();
+TEST(EpicsClientRandomTests,
+     CallingGeneratePVUpdateResultsInAPVUpdateInTheBuffer) {
+  // GIVEN an EpicsClient with a ring buffer
+  auto RingBuffer =
+      std::make_shared<Ring<std::unique_ptr<FlatBufs::EpicsPVUpdate>>>();
   auto EpicsClient = EpicsClient::EpicsClientRandom(RingBuffer);
 
+  // WHEN we call generateFakePVUpdate once
   EpicsClient.generateFakePVUpdate();
-  std::pair<int, std::unique_ptr<FlatBufs::EpicsPVUpdate>> GeneratedPV = RingBuffer->pop();
-  ASSERT_EQ(GeneratedPV.first, 0);  // 0 indicates success
+
+  // THEN there will be a single EpicsPVUpdate in the ring buffer
+  std::pair<int, std::unique_ptr<FlatBufs::EpicsPVUpdate>> GeneratedPV =
+      RingBuffer->pop();
+  ASSERT_EQ(GeneratedPV.first, 0); // 0 indicates success
+  std::pair<int, std::unique_ptr<FlatBufs::EpicsPVUpdate>>
+      PossibleSecondGeneratedPV = RingBuffer->pop();
+  ASSERT_EQ(PossibleSecondGeneratedPV.first,
+            1); // this time expect failure as only one should have been created
+}
+
+TEST(EpicsClientRandomTests,
+     CallingGeneratePVUpdateResultsInDifferentPVValues) {
+  // GIVEN an EpicsClient with a ring buffer
+  auto RingBuffer =
+      std::make_shared<Ring<std::unique_ptr<FlatBufs::EpicsPVUpdate>>>();
+  auto EpicsClient = EpicsClient::EpicsClientRandom(RingBuffer);
+
+  // WHEN we call generateFakePVUpdate once
+  EpicsClient.generateFakePVUpdate();
+  EpicsClient.generateFakePVUpdate();
+
+  // THEN there will be two EpicsPVUpdates in the ring buffer with different
+  // values
+  std::pair<int, std::unique_ptr<FlatBufs::EpicsPVUpdate>> FirstGeneratedPV =
+      RingBuffer->pop();
+  ASSERT_EQ(FirstGeneratedPV.first, 0); // 0 indicates success
+  std::pair<int, std::unique_ptr<FlatBufs::EpicsPVUpdate>> SecondGeneratedPV =
+      RingBuffer->pop();
+  ASSERT_EQ(SecondGeneratedPV.first, 0); // expect a second PVUpdate
+
+  double FirstGeneratedPVValue =
+      FirstGeneratedPV.second->epics_pvstr
+          ->getSubField<epics::pvData::PVDouble>("value")
+          ->get();
+  double SecondGeneratedPVValue =
+      SecondGeneratedPV.second->epics_pvstr
+          ->getSubField<epics::pvData::PVDouble>("value")
+          ->get();
+
+  // We should be generating random values for the PVs, so this tests the values
+  // we get are different
+  ASSERT_GT(abs(FirstGeneratedPVValue - SecondGeneratedPVValue), 0.00001);
 }
