2184447976227b8ffae832f31b51779cd9977f24
rerpha <jack.harper@stfc.ac.uk>
2018-06-21 09:33:12 +0100

    re #32 making system test work

--- system-tests/test_idle_pv_updates.py
+++ system-tests/test_idle_pv_updates.py
@@ -5,16 +5,14 @@ from helpers.epicshelpers import change_pv_value
 from time import sleep
 
 
-def test_forwarder_sends_idle_pv_updates(docker_compose_fake_epics):
+def test_forwarder_sends_idle_pv_updates(docker_compose_idle_updates):
     consumer = create_consumer()
     data_topic = "TEST_forwarderData_idle_updates"
     consumer.subscribe([data_topic])
     sleep(2)
-    change_pv_value("SIM:ParkAng", 10)
-    sleep(5)
     for i in range(3):
         msg = poll_for_valid_message(consumer).value()
         log_data_first = LogData.LogData.GetRootAsLogData(msg, 0)
         check_message_pv_name_and_value_type(log_data_first, Value.Value.Double, b'SIM:ParkAng')
-        check_double_value_and_equality(log_data_first, 10)
+        check_double_value_and_equality(log_data_first, 0)
         sleep(3)
