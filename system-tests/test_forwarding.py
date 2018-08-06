1a983601a98f85a37a952e242c05b12f2dadcff4
Jack Harper <jack.harper@stfc.ac.uk>
2018-08-06 11:30:08 +0100

    re #69 removing multiple pv same type test

--- system-tests/test_forwarding.py
+++ system-tests/test_forwarding.py
@@ -5,7 +5,7 @@ from helpers.kafka_helpers import create_consumer, poll_for_valid_message
 from helpers.flatbuffer_helpers import check_double_value_and_equality,\
     check_message_pv_name_and_value_type, create_flatbuffers_object, check_int_value_and_equality
 from helpers.epics_helpers import change_pv_value
-from PVs import PVDOUBLE, PVSTR, PVLONG, PVENUM, PVUSHORT, PVDOUBLE2
+from PVs import PVDOUBLE, PVSTR, PVLONG, PVENUM, PVUSHORT
 
 CONFIG_TOPIC = "TEST_forwarderConfig"
 
@@ -186,29 +186,6 @@ def test_forwarder_sends_pv_updates_single_pv_enum(docker_compose):
     log_data_second = LogData.LogData.GetRootAsLogData(second_msg, 0)
     check_message_pv_name_and_value_type(log_data_second, Value.Value.Int, PVENUM)
     check_int_value_and_equality(log_data_second, 1)
-    cons.close()
-
-
-def test_forwarder_updates_multiple_pv_same_type(docker_compose):
-    data_topic = "TEST_forwarderData_multiple_pv_same"
-
-    pvs = [PVDOUBLE, PVDOUBLE2]
-    prod = ProducerWrapper("localhost:9092", CONFIG_TOPIC, data_topic)
-    prod.add_config(pvs)
-
-    sleep(2)
-    cons = create_consumer()
-    sleep(2)
-    cons.subscribe([data_topic])
-    sleep(2)
-
-    first_msg = poll_for_valid_message(cons).value()
-    log_data_first = LogData.LogData.GetRootAsLogData(first_msg, 0)
-    check_message_pv_name_and_value_type(log_data_first, Value.Value.Double, PVDOUBLE)
-
-    second_msg = poll_for_valid_message(cons).value()
-    log_data_second = LogData.LogData.GetRootAsLogData(second_msg, 0)
-    check_message_pv_name_and_value_type(log_data_second, Value.Value.Double, PVDOUBLE2)
     cons.close()
 
 
