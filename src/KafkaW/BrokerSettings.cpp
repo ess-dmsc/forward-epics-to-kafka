b82a0330960c91e634eda9df062d310aed664113
Jack Harper <jack.harper@stfc.ac.uk>
2018-10-18 13:09:58 +0100

    re #187 moving static info into struct declaration rather than constructor

--- src/KafkaW/BrokerSettings.cpp
+++ src/KafkaW/BrokerSettings.cpp
@@ -5,24 +5,6 @@
 
 namespace KafkaW {
 
-BrokerSettings::BrokerSettings() {
-  KafkaConfiguration = {
-      {"api.version.request", "true"},
-      {"metadata.request.timeout.ms", "2000"}, // 2 Secs
-      {"socket.timeout.ms", "2000"},
-      {"message.max.bytes", "24117248"}, // 23 MiB
-      {"fetch.message.max.bytes", "24117248"},
-      {"receive.message.max.bytes", "24117248"},
-      {"queue.buffering.max.messages", "100000"},
-      {"queue.buffering.max.ms", "50"},
-      {"queue.buffering.max.kbytes", "819200"}, // 781.25 MiB
-      {"batch.num.messages", "100000"},
-      {"coordinator.query.interval.ms", "2000"},
-      {"heartbeat.interval.ms", "500"},     // 0.5 Secs
-      {"statistics.interval.ms", "600000"}, // 1 Min
-  };
-}
-
 void BrokerSettings::apply(rd_kafka_conf_t *RdKafkaConfiguration) {
   std::vector<char> ErrorString(256);
   for (const auto &ConfigurationItem : KafkaConfiguration) {
--- src/KafkaW/BrokerSettings.h
+++ src/KafkaW/BrokerSettings.h
@@ -10,12 +10,26 @@ namespace KafkaW {
 
 /// Collect options used to connect to the broker.
 
-class BrokerSettings {
+struct BrokerSettings {
 public:
-  BrokerSettings();
+  BrokerSettings() = default;
   void apply(rd_kafka_conf_t *RdKafkaConfiguration);
   std::string Address;
   int PollTimeoutMS = 100;
-  std::map<std::string, std::string> KafkaConfiguration;
+  std::map<std::string, std::string> KafkaConfiguration = {
+      {"api.version.request", "true"},
+      {"metadata.request.timeout.ms", "2000"}, // 2 Secs
+      {"socket.timeout.ms", "2000"},
+      {"message.max.bytes", "24117248"}, // 23 MiB
+      {"fetch.message.max.bytes", "24117248"},
+      {"receive.message.max.bytes", "24117248"},
+      {"queue.buffering.max.messages", "100000"},
+      {"queue.buffering.max.ms", "50"},
+      {"queue.buffering.max.kbytes", "819200"}, // 781.25 MiB
+      {"batch.num.messages", "100000"},
+      {"coordinator.query.interval.ms", "2000"},
+      {"heartbeat.interval.ms", "500"},     // 0.5 Secs
+      {"statistics.interval.ms", "600000"}, // 1 Min
+  };
 };
 }
