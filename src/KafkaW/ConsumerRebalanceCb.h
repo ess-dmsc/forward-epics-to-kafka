87914a8e3598f422f65af53ca950be4d6f887a86
mchorazak <michochozaku@gmail.com>
2018-12-04 14:41:25 +0000

    re #203 clearer error message

--- src/KafkaW/ConsumerRebalanceCb.h
+++ src/KafkaW/ConsumerRebalanceCb.h
@@ -6,8 +6,12 @@ public:
   void
   rebalance_cb(RdKafka::KafkaConsumer *consumer, RdKafka::ErrorCode err,
                std::vector<RdKafka::TopicPartition *> &partitions) override {
-    throw std::runtime_error(fmt::format("{}{}rebalancing cannot be done: {}",
-                                         consumer->name(), partitions.size(),
-                                         err));
+    std::string Topic = "unknown";
+    if (partitions.size() > 0)
+      Topic = partitions.at(0)->topic();
+    throw std::runtime_error(
+        fmt::format("CONSUMER: {}, TOPIC: {} ({} partitions), rebalancing "
+                    "cannot be done, ERROR: {}",
+                    consumer->name(), Topic, partitions.size(), err));
   }
 };
