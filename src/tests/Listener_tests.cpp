fd89ddb9d278b2ec7ecfaaa836f0ea98ac93da78
Jack Harper <jack.harper@stfc.ac.uk>
2019-01-07 10:17:07 +0000

    fixing unit tests

--- src/tests/Listener_tests.cpp
+++ src/tests/Listener_tests.cpp
@@ -1,8 +1,21 @@
 #include "../Config.h"
-#include "../KafkaW/ConsumerFake.h"
+#include "../KafkaW/Consumer.h"
 #include <CommandHandler.h>
 #include <gtest/gtest.h>
 #include <helper.h>
+
+namespace KafkaW {
+class ConsumerFake : public ConsumerInterface {
+public:
+  std::unique_ptr<ConsumerMessage> poll() override {
+    std::string Data = "1,2,3";
+    auto Message =
+        make_unique<KafkaW::ConsumerMessage>(Data, KafkaW::PollStatus::Msg);
+    return Message;
+  };
+  void addTopic(const std::string &Topic) override { UNUSED_ARG(Topic); };
+};
+}
 
 TEST(ListenerTest, successfully_create_listener_and_poll) {
   KafkaW::BrokerSettings bopt;
@@ -10,7 +23,8 @@ TEST(ListenerTest, successfully_create_listener_and_poll) {
 
   Forwarder::URI uri;
   Forwarder::Config::Listener listener(uri, std::move(FakeConsumer));
-  Forwarder::Forwarder *F;
-  Forwarder::ConfigCB config_cb(*F);
+  Forwarder::MainOpt Options;
+  Forwarder::Forwarder ForwarderInstance(Options);
+  Forwarder::ConfigCB config_cb(ForwarderInstance);
   ASSERT_NO_THROW(listener.poll(config_cb));
 }
