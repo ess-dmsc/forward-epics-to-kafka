6e0aea02a1f274a68a5946c7a4104774553c6752
Jonas Nilsson <jonas.nilsson@esss.se>
2019-01-22 23:59:59 +0000

    Added test. Fixed some things.

--- src/tests/BrokerSettings_tests.cpp
+++ src/tests/BrokerSettings_tests.cpp
@@ -1,19 +1,23 @@
-#include <gtest/gtest.h>
 #include "../KafkaW/BrokerSettings.h"
 #include "ConfStandIn.h"
+#include <gtest/gtest.h>
 #include <librdkafka/rdkafkacpp.h>
 
-using ::testing::AtLeast;
+using ::testing::Exactly;
+using ::testing::_;
+using ::testing::An;
 using namespace KafkaW;
 
 class BrokerSettingsTests : public ::testing::Test {
 protected:
-    void SetUp() override {}
-
+  void SetUp() override {}
 };
 
 TEST_F(BrokerSettingsTests, testSomething) {
-    BrokerSettings Settings;
-    ConfStandIn Conf;
-    Settings.apply(&Conf)
+  BrokerSettings Settings;
+  ConfStandIn Conf;
+  EXPECT_CALL(Conf, set(An<const std::string &>(), An<const std::string &>(),
+                        An<std::string &>()))
+      .Times(Exactly(Settings.KafkaConfiguration.size()));
+  Settings.apply(&Conf);
 }
--- src/tests/Producer_tests.cpp
+++ src/tests/Producer_tests.cpp
@@ -1,54 +1,64 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
 #include "../KafkaW/Producer.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 #include <librdkafka/rdkafkacpp.h>
 using ::testing::AtLeast;
 using namespace KafkaW;
 
 class ProducerTests : public ::testing::Test {
 protected:
-    void SetUp() override {}
-
+  void SetUp() override {}
 };
 
 class ProducerStandIn : public Producer {
 public:
-    ProducerStandIn(BrokerSettings Settings): Producer(Settings) {}
-    using Producer::id;
-    using Producer::ProducerPtr;
+  ProducerStandIn(BrokerSettings Settings) : Producer(Settings) {}
+  using Producer::id;
+  using Producer::ProducerPtr;
 };
 
 TEST_F(ProducerTests, creatingForwarderIncrementsForwarderCounter) {
-    BrokerSettings Settings{};
-    ProducerStandIn Producer1(Settings);
-    ProducerStandIn Producer2(Settings);
-    ASSERT_EQ(-1, Producer1.id - Producer2.id);
+  BrokerSettings Settings{};
+  ProducerStandIn Producer1(Settings);
+  ProducerStandIn Producer2(Settings);
+  ASSERT_EQ(-1, Producer1.id - Producer2.id);
 }
 
 class MockHandle : public RdKafka::Handle {
 public:
-    MOCK_CONST_METHOD0(name, const std::string());
-    MOCK_CONST_METHOD0(memberid, const std::string());
-    MOCK_METHOD1(poll, int(int));
-    MOCK_METHOD0(outq_len, int());
-    MOCK_METHOD4(metadata, RdKafka::ErrorCode(bool, const RdKafka::Topic*, RdKafka::Metadata**, int));
-    MOCK_METHOD1(pause, RdKafka::ErrorCode(std::vector<RdKafka::TopicPartition*>&));
-    MOCK_METHOD1(resume, RdKafka::ErrorCode(std::vector<RdKafka::TopicPartition*>&));
-    MOCK_METHOD5(query_watermark_offsets, RdKafka::ErrorCode(const std::string&, int32_t, int64_t*, int64_t*, int));
-    MOCK_METHOD4(get_watermark_offsets, RdKafka::ErrorCode(const std::string&, int32_t, int64_t*, int64_t*));
-    MOCK_METHOD2(offsetsForTimes, RdKafka::ErrorCode(std::vector<RdKafka::TopicPartition*>&, int));
-    MOCK_METHOD1(get_partition_queue, RdKafka::Queue*(const RdKafka::TopicPartition*));
-    MOCK_METHOD1(set_log_queue, RdKafka::ErrorCode(RdKafka::Queue*));
-    MOCK_METHOD0(yield, void());
-    MOCK_METHOD1(clusterid, const std::string(int));
-    MOCK_METHOD0(c_ptr, rd_kafka_s*());
+  MOCK_CONST_METHOD0(name, const std::string());
+  MOCK_CONST_METHOD0(memberid, const std::string());
+  MOCK_METHOD1(poll, int(int));
+  MOCK_METHOD0(outq_len, int());
+  MOCK_METHOD4(metadata, RdKafka::ErrorCode(bool, const RdKafka::Topic *,
+                                            RdKafka::Metadata **, int));
+  MOCK_METHOD1(pause,
+               RdKafka::ErrorCode(std::vector<RdKafka::TopicPartition *> &));
+  MOCK_METHOD1(resume,
+               RdKafka::ErrorCode(std::vector<RdKafka::TopicPartition *> &));
+  MOCK_METHOD5(query_watermark_offsets,
+               RdKafka::ErrorCode(const std::string &, int32_t, int64_t *,
+                                  int64_t *, int));
+  MOCK_METHOD4(get_watermark_offsets,
+               RdKafka::ErrorCode(const std::string &, int32_t, int64_t *,
+                                  int64_t *));
+  MOCK_METHOD2(offsetsForTimes,
+               RdKafka::ErrorCode(std::vector<RdKafka::TopicPartition *> &,
+                                  int));
+  MOCK_METHOD1(get_partition_queue,
+               RdKafka::Queue *(const RdKafka::TopicPartition *));
+  MOCK_METHOD1(set_log_queue, RdKafka::ErrorCode(RdKafka::Queue *));
+  MOCK_METHOD0(yield, void());
+  MOCK_METHOD1(clusterid, const std::string(int));
+  MOCK_METHOD0(c_ptr, rd_kafka_s *());
 };
 
 TEST_F(ProducerTests, callPollTest) {
-    BrokerSettings Settings{};
-    ProducerStandIn Producer1(Settings);
-    Producer1.ProducerPtr.reset(new MockHandle);
-    EXPECT_CALL(*dynamic_cast<MockHandle*>(Producer1.ProducerPtr.get()), poll(::testing::_)).Times(AtLeast(1));
-    Producer1.poll();
+  BrokerSettings Settings{};
+  ProducerStandIn Producer1(Settings);
+  Producer1.ProducerPtr.reset(new MockHandle);
+  EXPECT_CALL(*dynamic_cast<MockHandle *>(Producer1.ProducerPtr.get()),
+              poll(::testing::_))
+      .Times(AtLeast(1));
+  Producer1.poll();
 }
-
