b48e69e63597a977d3c72f6b0beed16dd73ab905
Jack Harper <jack.harper@stfc.ac.uk>
2019-01-25 14:44:08 +0000

    adding test for produce()

--- src/tests/Producer_tests.cpp
+++ src/tests/Producer_tests.cpp
@@ -3,28 +3,27 @@
 #include <gtest/gtest.h>
 #include <librdkafka/rdkafkacpp.h>
 using ::testing::AtLeast;
+using ::testing::Exactly;
+using ::testing::Return;
+using ::testing::_;
+
 using namespace KafkaW;
 
 class ProducerTests : public ::testing::Test {
 protected:
-  void SetUp() override {}
+  void SetUp() override {
+
+  }
 };
 
 class ProducerStandIn : public Producer {
 public:
-  ProducerStandIn(BrokerSettings Settings) : Producer(Settings) {}
+  explicit ProducerStandIn(const BrokerSettings &Settings) : Producer(Settings) {}
   using Producer::ProducerID;
   using Producer::ProducerPtr;
 };
 
-TEST_F(ProducerTests, creatingForwarderIncrementsForwarderCounter) {
-  BrokerSettings Settings{};
-  ProducerStandIn Producer1(Settings);
-  ProducerStandIn Producer2(Settings);
-  ASSERT_EQ(-1, Producer1.ProducerID - Producer2.ProducerID);
-}
-
-class MockHandle : public RdKafka::Handle {
+class MockProducer : public RdKafka::Producer {
 public:
   MOCK_CONST_METHOD0(name, const std::string());
   MOCK_CONST_METHOD0(memberid, const std::string());
@@ -51,19 +50,59 @@ public:
   MOCK_METHOD0(yield, void());
   MOCK_METHOD1(clusterid, const std::string(int));
   MOCK_METHOD0(c_ptr, rd_kafka_s *());
+  MOCK_METHOD2(create, RdKafka::Producer*(RdKafka::Conf*, std::string));
+  MOCK_METHOD7(produce, RdKafka::ErrorCode(RdKafka::Topic*, int32_t, int, void*, size_t, const std::string*, void*));
+  MOCK_METHOD8(produce, RdKafka::ErrorCode(RdKafka::Topic*, int32_t, int, void*, size_t, const void*, size_t, void*));
+  MOCK_METHOD9(produce, RdKafka::ErrorCode(const std::string, int32_t, int, void*, size_t, const void*, size_t, int64_t, void*));
+  MOCK_METHOD5(produce, RdKafka::ErrorCode(RdKafka::Topic*, int32_t, const std::vector<char>*, const std::vector<char>*, void*));
+  MOCK_METHOD1(flush, RdKafka::ErrorCode(int));
 };
+
+class FakeTopic : public RdKafka::Topic {
+public:
+    FakeTopic() = default;
+    ~FakeTopic() override = default;
+    const std::string name() const override {};
+    bool partition_available (int32_t partition) const override {return true;};
+    RdKafka::ErrorCode offset_store (int32_t partition, int64_t offset) override {};
+    struct rd_kafka_topic_s *c_ptr () override {};
+
+};
+
+TEST_F(ProducerTests, creatingForwarderIncrementsForwarderCounter) {
+    BrokerSettings Settings{};
+    ProducerStandIn Producer1(Settings);
+    ProducerStandIn Producer2(Settings);
+    ASSERT_EQ(-1, Producer1.ProducerID - Producer2.ProducerID);
+}
 
 TEST_F(ProducerTests, callPollTest) {
   BrokerSettings Settings{};
   ProducerStandIn Producer1(Settings);
-  Producer1.ProducerPtr.reset(new MockHandle);
-  EXPECT_CALL(*dynamic_cast<MockHandle *>(Producer1.ProducerPtr.get()),
-              poll(::testing::_))
+  Producer1.ProducerPtr.reset(new MockProducer);
+  EXPECT_CALL(*dynamic_cast<MockProducer *>(Producer1.ProducerPtr.get()),
+              poll(_))
       .Times(AtLeast(1));
 
-  EXPECT_CALL(*dynamic_cast<MockHandle *>(Producer1.ProducerPtr.get()),
+  EXPECT_CALL(*dynamic_cast<MockProducer *>(Producer1.ProducerPtr.get()),
               outq_len())
       .Times(AtLeast(1));
 
   Producer1.poll();
+}
+
+TEST_F(ProducerTests, produceReturnsNoErrorCodeIfMessageProduced) {
+    BrokerSettings Settings{};
+    ProducerStandIn Producer1(Settings);
+    Producer1.ProducerPtr.reset(new MockProducer);
+    EXPECT_CALL(*dynamic_cast<MockProducer *>(Producer1.ProducerPtr.get()), produce(_,_,_,_,_,_,_,_)).Times(Exactly(1)).WillOnce(Return(RdKafka::ERR_NO_ERROR));
+    ASSERT_EQ(Producer1.produce(new FakeTopic, 0, 0, nullptr, 0, nullptr, 0, nullptr), RdKafka::ErrorCode::ERR_NO_ERROR);
+}
+
+TEST_F(ProducerTests, produceReturnsErrorCodeIfMessageNotProduced) {
+    BrokerSettings Settings{};
+    ProducerStandIn Producer1(Settings);
+    Producer1.ProducerPtr.reset(new MockProducer);
+    EXPECT_CALL(*dynamic_cast<MockProducer *>(Producer1.ProducerPtr.get()), produce(_,_,_,_,_,_,_,_)).Times(Exactly(1)).WillOnce(Return(RdKafka::ERR__BAD_MSG));
+    ASSERT_EQ(Producer1.produce(new FakeTopic, 0, 0, nullptr, 0, nullptr, 0, nullptr), RdKafka::ErrorCode::ERR__BAD_MSG);
 }
