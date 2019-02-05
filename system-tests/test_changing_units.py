4ee9267aa01d3c37885dadfe0a095449010f027a
mchorazak <michochozaku@gmail.com>
2019-02-05 10:19:10 +0000

    re DM-1284 system test for changing units

---
+++ system-tests/test_changing_units.py
@@ -0,0 +1,32 @@
+from helpers.epics_helpers import change_pv_value
+from helpers.PVs import PVDOUBLE
+from time import sleep
+from helpers.producerwrapper import ProducerWrapper
+
+CONFIG_TOPIC = "TEST_forwarderConfig"
+
+
+def teardown_function(function):
+    """
+    Stops forwarder pv listening and resets any values in EPICS
+    :param docker_compose: test fixture to apply to
+    :return:
+    """
+    print("Resetting PVs", flush=True)
+    prod = ProducerWrapper("localhost:9092", CONFIG_TOPIC, "")
+    prod.stop_all_pvs()
+
+    defaults = {
+        PVDOUBLE: 0.0
+    }
+
+    for key, value in defaults.items():
+        change_pv_value(key, value)
+    sleep(3)
+
+
+def test_logs_error_when_units_change(docker_compose_units_change):
+    sleep(5)
+    change_pv_value ("{}.EGU".format(PVDOUBLE),"test")
+    change_pv_value ("{}.EGU".format(PVDOUBLE),"testdifferent")
+    sleep(5)
