a3be81ecd63734f6e9d86a583e8e68acdb6317a1
Jack Harper <jack.harper@stfc.ac.uk>
2019-02-05 15:42:35 +0000

    fixing system test

--- system-tests/test_changing_units.py
+++ system-tests/test_changing_units.py
@@ -6,25 +6,6 @@ from helpers.producerwrapper import ProducerWrapper
 CONFIG_TOPIC = "TEST_forwarderConfig"
 
 
-def teardown_function(function):
-    """
-    Stops forwarder pv listening and resets any values in EPICS
-    :param docker_compose: test fixture to apply to
-    :return:
-    """
-    print("Resetting PVs", flush=True)
-    prod = ProducerWrapper("localhost:9092", CONFIG_TOPIC, "")
-    prod.stop_all_pvs()
-
-    defaults = {
-        PVDOUBLE: 0.0
-    }
-
-    for key, value in defaults.items():
-        change_pv_value(key, value)
-    sleep(3)
-
-
 def test_logs_error_when_units_change(docker_compose_units_change):
     change_pv_value("{}.EGU".format(PVDOUBLE), "test1")
 
@@ -35,10 +16,10 @@ def test_logs_error_when_units_change(docker_compose_units_change):
 
     change_pv_value(PVDOUBLE, 10)
     sleep(5)
+    change_pv_value(PVDOUBLE, 0.0)
     prod = ProducerWrapper("localhost:9092", CONFIG_TOPIC, "")
     prod.exit_forwarder()
     sleep(10)
-
     found = False
     test_string = "Units changed in PV SIMPLE:DOUBLE from test1 to test2"
     with open("logs/forwarder_tests_units.log", 'r') as file:
