4cc20f775ae59c2f54536cbf7a88c0204591af90
mchorazak <michochozaku@gmail.com>
2019-02-27 12:06:10 +0000

    re DM-1168 change log level

--- src/CURLReporter.h
+++ src/CURLReporter.h
@@ -30,7 +30,7 @@ void send(fmt::memory_buffer &MemoryWriter, std::string const &URL) {
     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, MemoryWriter.data());
     res = curl_easy_perform(curl);
     if (res != CURLE_OK) {
-      LOG(Sev::Notice, "curl_easy_perform() failed: {}",
+      LOG(spdlog::level::warn, "curl_easy_perform() failed: {}",
           curl_easy_strerror(res));
     }
   }
