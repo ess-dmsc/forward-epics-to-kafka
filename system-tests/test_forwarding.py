905017a3be24b497d0e17c27a9c629b062e48c53
Jack Harper <jack.harper@stfc.ac.uk>
2019-04-03 16:10:01 +0100

    making tests run faster - no teardown is needed in the config file test as the forwarder container will be terminated after the test runs anyway.

--- system-tests/test_forwarding.py
+++ system-tests/test_forwarding.py
@@ -1,35 +1,11 @@
-﻿from helpers.producerwrapper import ProducerWrapper
-from helpers.f142_logdata.Value import Value
+﻿from helpers.f142_logdata.Value import Value
 from time import sleep
 from helpers.kafka_helpers import create_consumer, poll_for_valid_message
 from helpers.flatbuffer_helpers import check_expected_values
 from helpers.epics_helpers import change_pv_value
-from helpers.PVs import PVDOUBLE, PVSTR, PVLONG, PVENUM
-
+from helpers.PVs import PVDOUBLE
 
 CONFIG_TOPIC = "TEST_forwarderConfig"
-
-
-def teardown_function(function):
-    """
-    Stops forwarder pv listening and resets any values in EPICS
-    """
-    print("Resetting PVs", flush=True)
-    prod = ProducerWrapper("localhost:9092", CONFIG_TOPIC, "")
-    prod.stop_all_pvs()
-
-    defaults = {
-        PVDOUBLE: 0.0,
-        # We have to use this as the second parameter for caput gets parsed as empty so does not change the value of
-        # the PV
-        PVSTR: "\"\"",
-        PVLONG: 0,
-        PVENUM: "INIT"
-    }
-
-    for key, value in defaults.items():
-        change_pv_value(key, value)
-    sleep(3)
 
 
 def test_config_file_channel_created_correctly(docker_compose):
@@ -55,5 +31,3 @@ def test_config_file_channel_created_correctly(docker_compose):
     second_msg = poll_for_valid_message(cons)
     check_expected_values(second_msg, Value.Double, PVDOUBLE, 10.0)
     cons.close()
-
-
