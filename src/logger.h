9d1e678a155e05afbb102ea3f305163e3d456d0d
mchorazak <michochozaku@gmail.com>
2019-04-04 11:57:00 +0100

    DM-1140 remove clang-format on/off - superfluous since spdlog 1.3.1

--- src/logger.h
+++ src/logger.h
@@ -1,12 +1,10 @@
 #pragma once
 #include <fmt/format.h>
-#include <string>
-// clang-format off
-#include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/graylog_sink.h>
-// clang-format on
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+#include <string>
 #define UNUSED_ARG(x) (void)x;
 
 void setUpInitializationLogging();
