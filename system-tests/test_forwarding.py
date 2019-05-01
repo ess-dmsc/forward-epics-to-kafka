c45b86038923de5a262a48096c6f7847ca9c664c
Matthew D Jones <matthew.d.jones@tessella.com>
2019-05-01 11:19:57 +0100

    Add comment to test

--- system-tests/test_forwarding.py
+++ system-tests/test_forwarding.py
@@ -27,6 +27,8 @@ def test_config_file_channel_created_correctly(docker_compose):
     first_msg, msg_key = poll_for_valid_message(cons)
     check_expected_values(first_msg, Value.Double, PVDOUBLE, 0.0)
     assert(msg_key == b'SIMPLE:DOUBLE'), 'Message key expected to be the same as the PV name'
+    # We set the message key to be the PV name so that all messages from the same PV are sent to
+    # the same partition by Kafka. This ensures that the order of these messages is maintained to the consumer.
 
     # Check the new value is forwarded
     second_msg, _ = poll_for_valid_message(cons)
