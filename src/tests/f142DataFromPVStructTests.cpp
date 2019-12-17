f8c8f17bf4cba44708a3a791232ecd18cba5bfaf
Matthew D Jones <matthew.d.jones@tessella.com>
2019-12-17 15:37:44 +0000

    Add test for extracting alarm severity from epics pv struct

--- src/tests/f142DataFromPVStructTests.cpp
+++ src/tests/f142DataFromPVStructTests.cpp
@@ -5,7 +5,9 @@
 #include <pv/pvTimeStamp.h>
 
 epics::pvData::PVStructure::shared_pointer
-CreateTestPVStructWithAlarm(std::string const &AlarmMessage) {
+CreateTestPVStructWithAlarm(std::string const &AlarmMessage,
+                            epics::pvData::AlarmSeverity AlarmSeverityLevel =
+                                epics::pvData::AlarmSeverity::noAlarm) {
   auto Builder = epics::nt::NTScalar::createBuilder();
   Builder->addAlarm();
   auto PVStruct =
@@ -21,7 +23,7 @@ CreateTestPVStructWithAlarm(std::string const &AlarmMessage) {
   auto AlarmData = epics::pvData::Alarm();
   AlarmData.setMessage(AlarmMessage);
   AlarmData.setStatus(epics::pvData::AlarmStatus::noStatus);
-  AlarmData.setSeverity(epics::pvData::AlarmSeverity::majorAlarm);
+  AlarmData.setSeverity(AlarmSeverityLevel);
   auto ConstructedPVAlarm = epics::pvData::PVAlarm();
   PVStruct->attachAlarm(ConstructedPVAlarm);
   ConstructedPVAlarm.set(AlarmData);
@@ -44,4 +46,28 @@ TEST(
   testAlarmStatusFromAlarmMessage("LOW_ALARM", AlarmStatus::LOW);
   testAlarmStatusFromAlarmMessage("READ_ACCESS_ALARM",
                                   AlarmStatus::READ_ACCESS);
+}
+
+void testAlarmSeverityFromAlarmStruct(
+    epics::pvData::AlarmSeverity AlarmSeverityInStruct,
+    AlarmSeverity ExpectedAlarmSeverity) {
+  auto PVStruct =
+      CreateTestPVStructWithAlarm("NO_ALARM", AlarmSeverityInStruct);
+  auto AlarmStatusFromStruct = FlatBufs::f142::getAlarmSeverity(PVStruct);
+  ASSERT_EQ(AlarmStatusFromStruct, ExpectedAlarmSeverity);
+}
+
+TEST(
+    f142Test,
+    test_getAlarmSeverity_returns_expected_severity_based_on_alarm_in_pv_structure) {
+  testAlarmSeverityFromAlarmStruct(epics::pvData::AlarmSeverity::noAlarm,
+                                   AlarmSeverity::NO_ALARM);
+  testAlarmSeverityFromAlarmStruct(epics::pvData::AlarmSeverity::minorAlarm,
+                                   AlarmSeverity::MINOR);
+  testAlarmSeverityFromAlarmStruct(epics::pvData::AlarmSeverity::majorAlarm,
+                                   AlarmSeverity::MAJOR);
+  testAlarmSeverityFromAlarmStruct(epics::pvData::AlarmSeverity::invalidAlarm,
+                                   AlarmSeverity::INVALID);
+  testAlarmSeverityFromAlarmStruct(epics::pvData::AlarmSeverity::undefinedAlarm,
+                                   AlarmSeverity::INVALID);
 }
