649516ca9f8100d961b85fa166c2d9e611f71c97
Matthew D Jones <matthew.d.jones@tessella.com>
2019-12-17 14:34:15 +0000

    Add test for extracting alarm status from epics pv structure

--- src/tests/f142DataFromPVStructTests.cpp
+++ src/tests/f142DataFromPVStructTests.cpp
@@ -1,10 +1,11 @@
 #include "schemas/f142/DataFromPVStruct.h"
 #include <gtest/gtest.h>
-#include <pv/pvAlarm.h>
 #include <pv/nt.h>
+#include <pv/pvAlarm.h>
 #include <pv/pvTimeStamp.h>
 
-epics::pvData::PVStructure::shared_pointer CreateTestPVStructWithAlarm(std::string const &AlarmMessage) {
+epics::pvData::PVStructure::shared_pointer
+CreateTestPVStructWithAlarm(std::string const &AlarmMessage) {
   auto Builder = epics::nt::NTScalar::createBuilder();
   Builder->addAlarm();
   auto PVStruct =
@@ -28,8 +29,18 @@ epics::pvData::PVStructure::shared_pointer CreateTestPVStructWithAlarm(std::stri
   return PVStruct->getPVStructure();
 }
 
-TEST(f142Test, some_test) {
-  auto PVStruct = CreateTestPVStructWithAlarm("HIHI_ALARM");
-  // auto AlarmStatusFromStruct = FlatBufs::f142::getAlarmStatus(PVStruct);
-  
+void testAlarmStatusFromAlarmMessage(std::string const &AlarmMessage,
+                                     AlarmStatus ExpectedAlarmStatus) {
+  auto PVStruct = CreateTestPVStructWithAlarm(AlarmMessage);
+  auto AlarmStatusFromStruct = FlatBufs::f142::getAlarmStatus(PVStruct);
+  ASSERT_EQ(AlarmStatusFromStruct, ExpectedAlarmStatus);
+}
+
+TEST(
+    f142Test,
+    test_getAlarmStatus_returns_expected_status_based_on_alarm_message_in_pv_structure) {
+  testAlarmStatusFromAlarmMessage("NO_ALARM", AlarmStatus::NO_ALARM);
+  testAlarmStatusFromAlarmMessage("HIHI_ALARM", AlarmStatus::HIHI);
+  testAlarmStatusFromAlarmMessage("LOW_ALARM", AlarmStatus::LOW);
+  testAlarmStatusFromAlarmMessage("READ_ACCESS_ALARM", AlarmStatus::READ_ACCESS);
 }
