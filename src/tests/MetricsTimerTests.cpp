083f3bb5df8225b7503f518f2e1bee6837d4b16b
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-01-16 16:06:57 +0000

    simple test

---
+++ src/tests/MetricsTimerTests.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+class MetricsTimerTest : public ::testing::Test {};
+
+namespace Forwarder {
+TEST(MetricsTimerTest, BasicTest) { ASSERT_TRUE(true); }
+
+} // namespace Forwarder
