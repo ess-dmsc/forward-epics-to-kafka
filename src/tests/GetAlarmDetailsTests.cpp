a72ca90d90eb398efd7213028b861e7d5ad9cb5b
Matthew D Jones <matthew.d.jones@tessella.com>
2020-02-28 13:47:11 +0000

    Unit tests to cover getting alarm status when no alarm in pv struct

--- src/tests/GetAlarmDetailsTests.cpp
+++ src/tests/GetAlarmDetailsTests.cpp
@@ -4,12 +4,8 @@
 #include <pv/pvAlarm.h>
 #include <pv/pvTimeStamp.h>
 
-epics::pvData::PVStructure::shared_pointer
-CreateTestPVStructWithAlarm(std::string const &AlarmMessage,
-                            epics::pvData::AlarmSeverity AlarmSeverityLevel =
-                                epics::pvData::AlarmSeverity::noAlarm) {
-  auto Builder = epics::nt::NTScalar::createBuilder();
-  Builder->addAlarm();
+epics::nt::NTScalarPtr
+createPVStructure(epics::nt::NTScalarBuilderPtr const &Builder) {
   auto PVStruct =
       Builder->value(epics::pvData::pvDouble)->addTimeStamp()->create();
   auto ValueField = PVStruct->getValue<epics::pvData::PVDouble>();
@@ -18,6 +14,16 @@ CreateTestPVStructWithAlarm(std::string const &AlarmMessage,
   TS.getCurrent();
   epics::pvData::PVTimeStamp pvTS;
   PVStruct->attachTimeStamp(pvTS);
+  return PVStruct;
+}
+
+epics::pvData::PVStructure::shared_pointer
+createTestPVStructWithAlarm(std::string const &AlarmMessage,
+                            epics::pvData::AlarmSeverity AlarmSeverityLevel =
+                                epics::pvData::AlarmSeverity::noAlarm) {
+  auto Builder = epics::nt::NTScalar::createBuilder();
+  Builder->addAlarm();
+  auto PVStruct = createPVStructure(Builder);
 
   // Construct an alarm structure
   auto AlarmData = epics::pvData::Alarm();
@@ -31,15 +37,21 @@ CreateTestPVStructWithAlarm(std::string const &AlarmMessage,
   return PVStruct->getPVStructure();
 }
 
+epics::pvData::PVStructure::shared_pointer createTestPVStructWithoutAlarm() {
+  auto Builder = epics::nt::NTScalar::createBuilder();
+  auto PVStruct = createPVStructure(Builder);
+  return PVStruct->getPVStructure();
+}
+
 void testAlarmStatusFromAlarmMessage(std::string const &AlarmMessage,
                                      AlarmStatus ExpectedAlarmStatus) {
-  auto PVStruct = CreateTestPVStructWithAlarm(AlarmMessage);
+  auto PVStruct = createTestPVStructWithAlarm(AlarmMessage);
   auto AlarmStatusFromStruct = FlatBufs::f142::getAlarmStatus(PVStruct);
   ASSERT_EQ(AlarmStatusFromStruct, ExpectedAlarmStatus);
 }
 
 TEST(
-    f142Test,
+    GetAlarmDetailsTest,
     test_getAlarmStatus_returns_expected_status_based_on_alarm_message_in_pv_structure) {
   testAlarmStatusFromAlarmMessage("NO_ALARM", AlarmStatus::NO_ALARM);
   testAlarmStatusFromAlarmMessage("HIHI_ALARM", AlarmStatus::HIHI);
@@ -48,17 +60,24 @@ TEST(
                                   AlarmStatus::READ_ACCESS);
 }
 
+TEST(GetAlarmDetailsTest,
+     test_getAlarmStatus_returns_no_alarm_if_no_alarm_struct_in_pv) {
+  auto PVStruct = createTestPVStructWithoutAlarm();
+  auto AlarmStatusFromStruct = FlatBufs::f142::getAlarmStatus(PVStruct);
+  ASSERT_EQ(AlarmStatusFromStruct, AlarmStatus::NO_ALARM);
+}
+
 void testAlarmSeverityFromAlarmStruct(
     epics::pvData::AlarmSeverity AlarmSeverityInStruct,
     AlarmSeverity ExpectedAlarmSeverity) {
   auto PVStruct =
-      CreateTestPVStructWithAlarm("NO_ALARM", AlarmSeverityInStruct);
-  auto AlarmStatusFromStruct = FlatBufs::f142::getAlarmSeverity(PVStruct);
-  ASSERT_EQ(AlarmStatusFromStruct, ExpectedAlarmSeverity);
+      createTestPVStructWithAlarm("NO_ALARM", AlarmSeverityInStruct);
+  auto AlarmSeverityFromStruct = FlatBufs::f142::getAlarmSeverity(PVStruct);
+  ASSERT_EQ(AlarmSeverityFromStruct, ExpectedAlarmSeverity);
 }
 
 TEST(
-    f142Test,
+    GetAlarmDetailsTest,
     test_getAlarmSeverity_returns_expected_severity_based_on_alarm_in_pv_structure) {
   testAlarmSeverityFromAlarmStruct(epics::pvData::AlarmSeverity::noAlarm,
                                    AlarmSeverity::NO_ALARM);
@@ -70,4 +89,11 @@ TEST(
                                    AlarmSeverity::INVALID);
   testAlarmSeverityFromAlarmStruct(epics::pvData::AlarmSeverity::undefinedAlarm,
                                    AlarmSeverity::INVALID);
+}
+
+TEST(GetAlarmDetailsTest,
+     test_getAlarmSeverity_returns_no_alarm_if_no_alarm_struct_in_pv) {
+  auto PVStruct = createTestPVStructWithoutAlarm();
+  auto AlarmSeverityFromStruct = FlatBufs::f142::getAlarmSeverity(PVStruct);
+  ASSERT_EQ(AlarmSeverityFromStruct, AlarmSeverity::NO_ALARM);
 }
